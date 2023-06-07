d60886fdbb1463bf16b67255e425b7eddb5afe73
Sophivorus <schenonef@gmail.com>
2023-06-07 13:10:50 -0700

    Skip empty paragraphs
    Change-Id: Ie93fe607768051ef63a392ad99ee37125b422d74

--- wikiedit.js
+++ wikiedit.js
@@ -53,6 +53,11 @@ window.WikiEdit = {
 	addEditButton: function () {
 		var $paragraph = $( this );
 
+		// Skip empty paragraphs
+		if ( !$paragraph.text().trim() ) {
+			return;
+		}
+
 		// Make the button
 		var path = '<path fill="currentColor" d="M16.77 8l1.94-2a1 1 0 0 0 0-1.41l-3.34-3.3a1 1 0 0 0-1.41 0L12 3.23zm-5.81-3.71L1 14.25V19h4.75l9.96-9.96-4.75-4.75z"></path>';
 		var icon = '<svg width="14" height="14" viewBox="0 0 20 20">' + path + '</svg>';
